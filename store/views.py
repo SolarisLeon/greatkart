13fc33fdfcbf0f4990a9217ac5a37ff6fd756064
Solias Leon <surajs640@gmail.com>
2023-12-18 11:47:15 +0530

    search bar functionality fixed

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -48,3 +49,18 @@ def product_details(request, category_slug, product_slug):
         "in_cart": in_cart,
     }
     return render(request, 'store/product_details.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by(
+                '-created_date').filter(Q(product_descriptions__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
